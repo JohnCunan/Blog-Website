8e2cf9a1786151d84f02df73e75e861aaeaf4bf4
JohnCunan <76254034+JohnCunan@users.noreply.github.com>
2022-06-21 18:58:09 +0800

    Dark Mode & Pop Up
    Dark mode button added
    Pop up and Pop Our added
    
    a total of 4 events (For Homework)

--- Scripts/main_page.js
+++ Scripts/main_page.js
@@ -1,5 +1,5 @@
 //BACK TO TOP BUTTON
-topButton = document.getElementById("back-To-Top-Btn");
+const topButton = document.getElementById("back-To-Top-Btn");
 
 window.onscroll = function () { scrollFunction() };
 
@@ -13,4 +13,20 @@ function scrollFunction() {
 
 function topFunction() {
     window.scrollTo(0, 0)
+}
+
+//DARK MODE BUTTON
+function changeTheme() {
+    document.body.classList.toggle("dark")
+}
+
+//POP UP
+function showPopUp() {
+    var popup = document.getElementById("darkPopUp");
+    popup.classList.toggle("show");
+}
+
+function hidePopUp() {
+    var pophide = document.getElementById("darkPopUp")
+    pophide.classList.toggle("hide");
 }
