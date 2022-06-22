449b5dc75bef8720ba07744d3ccb3f74431d65bf
JohnCunan <76254034+JohnCunan@users.noreply.github.com>
2022-06-22 14:15:03 +0800

    pop up modal onload added

---
+++ Scripts/modal-script.js
@@ -0,0 +1,13 @@
+//INITIAL MODAL
+
+var mod = document.getElementById("initPop")
+
+function showModal() {
+    mod.style.opacity = "1"
+}
+
+function closeModal() {
+    mod.style.opacity = "0"
+    mod.style.pointerEvents = "none"
+
+}
